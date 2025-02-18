6007c020cbd5198fd068354156a68d8f4b605c68
Marcio Bueno <marciobueno@gmail.com>
2025-02-18 23:45:26 +0000

    Programa 01 - exemplo de funções

---
+++ ex01-hello-world/Exemplo01.java
@@ -0,0 +1,14 @@
+public class Exemplo01 {
+  public static void main(String[] args) {
+    // System.out.println("Olá, Turma!");
+    System.out.println("O dobro de 10.7 é " + dobro(10.7));
+  }
+
+  public static int dobro(int num) {
+    return 2 * num;
+  }
+
+  public static double dobro(double num) {
+    return 2 * num;
+  }
+}
