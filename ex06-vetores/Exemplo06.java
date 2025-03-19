c51831dd8c4a837052c3ed52a2434409756c98c1
Marcio Bueno <marciobueno@gmail.com>
2025-03-19 00:07:43 +0000

    Exemplo06 - vetores - função busca

--- ex06-vetores/Exemplo06.java
+++ ex06-vetores/Exemplo06.java
@@ -5,6 +5,8 @@ public class Exemplo06 {
     public static void main(String[] args) {
         double[] notas = new double[5];
         double somatorio, media;
+        double nota;
+        int pos;
 
         // Digitação de todas as notas
         for (int i = 0; i < notas.length; i += 1) {
@@ -32,5 +34,29 @@ public class Exemplo06 {
                 System.out.println((i + 1) + "ª nota: " + notas[i]);
             }
         }
+
+        // encontrar um valor dentro do array
+        System.out.print("\nDigite uma nota a ser pesquisada: ");
+        nota = input.nextDouble();
+        while (nota >= 0.0) {
+            pos = busca(notas, nota);
+            if (pos != -1) {
+                System.out.println("A nota " + nota +
+                        " foi encontrada na " + (pos + 1) + "ª posição");
+            } else {
+                System.out.println("A nota " + nota + " não foi encontrada");
+            }
+            System.out.print("\nDigite uma nota a ser pesquisada (< 0.0 para finalizar): ");
+            nota = input.nextDouble();
+        }
+    }
+
+    public static int busca(double[] v, double x) {
+        for (int i = 0; i < v.length; i += 1) {
+            if (v[i] == x) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
