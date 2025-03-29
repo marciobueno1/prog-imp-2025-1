33ac545eb1298058ec5e9854998e20db10d89b1d
Marcio Bueno <marciobueno@gmail.com>
2025-03-29 00:43:15 +0000

    exemplo 06 - testando se o vetor está ordenado ou não.

--- ex06-vetores/Exemplo06.java
+++ ex06-vetores/Exemplo06.java
@@ -109,4 +109,25 @@ public class Exemplo06 {
         }
         return -1;
     }
+
+    public static boolean estaOrdenado(double[] v) {
+        boolean result = true;
+        // testando se o vetor está ordenado de forma crescente
+        for (int i = 1; i < v.length; i += 1) {
+            if (v[i] < v[i - 1]) {
+                result = false;
+                break;
+            }
+        }
+        if (result) {
+            return true;
+        }
+        // testando se o vetor está ordenado de forma decrescente
+        for (int i = 1; i < v.length; i += 1) {
+            if (v[i] > v[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
