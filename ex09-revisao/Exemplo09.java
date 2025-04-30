c976750ec12a51dc8443c47fc4ee542a994f0193
Marcio Bueno <marciobueno@gmail.com>
2025-04-30 00:41:17 +0000

    Resolução questões 5 e 6 da prova

--- ex09-revisao/Exemplo09.java
+++ ex09-revisao/Exemplo09.java
@@ -14,6 +14,8 @@ public class Exemplo09 {
       System.out.println("3. Ordenar o Vetor");
       System.out.println("4. Pesquisar no Vetor");
       System.out.println("5. Calcular a média");
+      System.out.println("6. Buscar menor valor par do vetor");
+      System.out.println("7. Imprimir tabela de frequência");
       System.out.print("Digite sua opção (0 p/ sair): ");
       opcao = input.nextInt();
 
@@ -45,6 +47,25 @@ public class Exemplo09 {
           
         case 5:
           System.out.println("A média do vetor é " + calcularMedia(vetor1, qtdVetor1));
+          break;
+
+        case 6:
+          int posMenorPar1 = menorPar(vetor1, qtdVetor1);
+          int posMenorPar2 = menorParVersao2(vetor1, qtdVetor1);
+          if (posMenorPar1 != -1) {
+            System.out.println("[1] Menor par está na posição " + posMenorPar1 + " do vetor");
+          } else {
+            System.out.println("[1] Não tem nenhum par no vetor");
+          }
+          if (posMenorPar2 != -1) {
+            System.out.println("[2] Menor par está na posição " + posMenorPar2 + " do vetor");
+          } else {
+            System.out.println("[2] Não tem nenhum par no vetor");
+          }
+          break;
+        
+        case 7:
+          imprimirDuplicados(vetor1, qtdVetor1);
           break;
 
         default:
@@ -114,5 +135,50 @@ public class Exemplo09 {
       soma += v[i];
     }
     return (double) soma / (double) n;
+  }
+
+  public static int menorPar(int[] v, int n) {
+    int i = 0, posMenorPar;
+    while (i < n && v[i] % 2 != 0) {
+      i += 1;
+    }
+    if (i == n) {
+      return -1;
+    }
+    posMenorPar = i;
+    for (i = i + 1; i < n; i += 1) {
+      if (v[i] % 2 == 0 && v[i] < v[posMenorPar]) {
+        posMenorPar = i;
+      }
+    }
+    return posMenorPar;
+  }
+
+  public static int menorParVersao2(int[] v, int n) {
+    int posMenorPar = 0;
+    for (int i = 1; i < n; i += 1) {
+      if (v[i] % 2 == 0 && (v[i] < v[posMenorPar] || v[posMenorPar] % 2 != 0)) {
+        posMenorPar = i;
+      }
+    }
+    if (v[posMenorPar] % 2 == 0) {
+      return posMenorPar;
+    }
+    return -1;
+  }
+
+  public static void imprimirDuplicados(int[] v, int n) {
+    int qtd = 1;
+    insercaoDireta(v, n);
+    System.out.println("NUM\tQTD");
+    for (int i = 1; i < n; i += 1) {
+      if (v[i] == v[i - 1]) {
+        qtd += 1;
+      } else {
+        System.out.println(v[i - 1] + "\t" + qtd);
+        qtd = 1;
+      }
+    }
+    System.out.println(v[n - 1] + "\t" + qtd);
   }
 }
