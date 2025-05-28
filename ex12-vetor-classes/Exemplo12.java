d4195f14339591e4f4dd474ffcefb9b302716616
Marcio Bueno <marciobueno@gmail.com>
2025-05-28 00:37:32 +0000

    método bolha (ordenação pela idade e pelo nome)
    busca binária (por idade e por nome)

--- ex12-vetor-classes/Exemplo12.java
+++ ex12-vetor-classes/Exemplo12.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Exemplo12 {
-  public static final int TAM = 5;
+  public static final int TAM = 10;
   public static Scanner input = new Scanner(System.in);
 
   public static void main(String[] args) {
+    String nome;
+    int pos;
     Pessoa[] pessoas = new Pessoa[TAM];
 
     for (int i = 0; i < pessoas.length; i += 1) {
@@ -24,6 +26,31 @@ public class Exemplo12 {
 
     double mediaIdade = calcularMediaIdade(pessoas, pessoas.length);
     System.out.println("A média de idade destas pessoas é " + mediaIdade);
+
+    metodoBolhaPorIdade(pessoas, pessoas.length);
+
+    System.out.println("\n\n-------IMPRESSÃO APÓS ORDENAÇÃO BOLHA POR IDADE-------");
+    for (int i = 0; i < pessoas.length; i += 1) {
+      imprimirPessoa(pessoas[i]);
+    }
+
+    // metodoBolhaPorNome(pessoas, pessoas.length);
+
+    // System.out.println("\n\n-------IMPRESSÃO APÓS ORDENAÇÃO BOLHA POR NOME-------");
+    // for (int i = 0; i < pessoas.length; i += 1) {
+    //   imprimirPessoa(pessoas[i]);
+    // }
+
+    // System.out.println("\n\n----------Busca binária por nome----------");
+    // System.out.print("Digite um nome para ser buscado: ");
+    // nome = input.nextLine();
+    // pos = buscaBinariaPorNome(pessoas, pessoas.length, nome);
+    // if (pos != -1) {
+    //   System.out.print("Pessoa encontrada: ");
+    //   imprimirPessoa(pessoas[pos]);
+    // } else {
+    //   System.out.println("Pessoa não encontrada!");
+    // }
   }
 
   public static void lerPessoa(Pessoa p) {
@@ -67,5 +94,73 @@ public class Exemplo12 {
       soma += v[i].idade;
     }
     return soma / tam;
+  }
+
+  public static void metodoBolhaPorIdade(Pessoa[] v, int tam) {
+    int fim = tam - 2, pos = 0;
+    boolean trocou = true;
+    Pessoa chave;
+    while (trocou) {
+      trocou = false;
+      for (int i = 0; i <= fim; i++) {
+        if (v[i].idade > v[i + 1].idade) {
+          chave = v[i];
+          v[i] = v[i + 1];
+          v[i + 1] = chave;
+          pos = i;
+          trocou = true;
+        }
+      }
+      fim = pos - 1;
+    }
+  }
+  
+  public static int buscaBinariaPorIdade(Pessoa[] v, int tam, int x) {
+    int inicio = 0, fim = tam - 1, meio;
+    while (inicio <= fim) {
+        meio = (inicio + fim) / 2;
+        if (x == v[meio].idade) {
+            return meio;
+        } else if (x < v[meio].idade) {
+            fim = meio - 1;
+        } else {
+            inicio = meio + 1;
+        }
+    }
+    return -1;
+  }
+
+  public static void metodoBolhaPorNome(Pessoa[] v, int tam) {
+    int fim = tam - 2, pos = 0;
+    boolean trocou = true;
+    Pessoa chave;
+    while (trocou) {
+      trocou = false;
+      for (int i = 0; i <= fim; i++) {
+        if (v[i].nome.toUpperCase().compareTo(v[i + 1].nome.toUpperCase()) > 0) {
+          chave = v[i];
+          v[i] = v[i + 1];
+          v[i + 1] = chave;
+          pos = i;
+          trocou = true;
+        }
+      }
+      fim = pos - 1;
+    }
+  }
+
+  public static int buscaBinariaPorNome(Pessoa[] v, int tam, String x) {
+    int inicio = 0, fim = tam - 1, meio;
+    while (inicio <= fim) {
+        meio = (inicio + fim) / 2;
+        if (x.toUpperCase().compareTo(v[meio].nome.toUpperCase()) == 0) {
+            return meio;
+        } else if (x.toUpperCase().compareTo(v[meio].nome.toUpperCase()) < 0) {
+            fim = meio - 1;
+        } else {
+            inicio = meio + 1;
+        }
+    }
+    return -1;
   }
 }
